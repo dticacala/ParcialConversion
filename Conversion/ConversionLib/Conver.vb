bf7f940f358f101136a4e97a4bdb2d6f0d13c08c
Daysi Rosa <daysi.2t@gmail.com>
2018-11-22 12:47:05 -0500

    version 1

---
+++ Conversion/ConversionLib/Conver.vb
@@ -0,0 +1,6 @@
+﻿Imports System.Drawing
+Public Class Conver
+    Public Function polarToCartesian(r As Double, theta As Double) As Point
+
+    End Function
+End Class
---
+++ Conversion/UnitTestConversion/UnitTest.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports System.Drawing
+Imports ConversionLib
+
+<TestClass()> Public Class UnitTest
+
+    <TestMethod()> Public Sub TestMethod1()
+    End Sub
+
+End Class
